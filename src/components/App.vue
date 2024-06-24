403e1dae38b9e13668ae93ab5f4a4e8c9a642f08
iahub4metra <buhai.artem7@gmail.com>
2024-06-24 21:13:31 +0100

    transactions

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,6 +2,7 @@
   <div>
     <Profile :userData="userData"/>
     <FriendList :friends="friends"/>
+    <TransactionHistory :transactions="transactions"/>
   </div>
 </template>
 <script setup>
@@ -9,4 +10,6 @@ import Profile from "./profile/Profile.vue"
 import FriendList from "./friendList/FriendList.vue"
 import userData from "../assets/userData.json"
 import friends from "../assets/friends.json"
+import TransactionHistory from "./transactionHistory/TransactionHistory.vue"
+import transactions from "../assets/transactions.json"
 </script>
---
+++ src/components/transactionHistory/TransactionHistory.vue
@@ -0,0 +1,29 @@
+<template>
+    <table :class="css.transactionHistoryTable">
+        <thead>
+            <tr>
+                <th>Type</th>
+                <th>Amount</th>
+                <th>Currency</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr v-for="transaction in transactions" :key="transaction.id">
+                <td>{{capitalizeFirstLetter(transaction.type)}}</td>
+                <td>{{transaction.amount}}</td>
+                <td>{{transaction.currency}}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+<script setup>
+import {defineProps} from "vue"
+import css from "./TransactionHistory.module.css"
+const props = defineProps({
+    transactions: Array,
+})
+const capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+</script>
